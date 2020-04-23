b1b1745ffa71bfb5737692f556b9ce68f5c38edf
Matesanz <Matesanz.Cuadrado@gmail.com>
2020-04-23 10:35:12 +0200

    Simplified Simple GAN implementation

--- models/simple_gan.py
+++ models/simple_gan.py
@@ -4,177 +4,184 @@ import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Input, LeakyReLU, BatchNormalization
 from tensorflow.keras.optimizers import Adam, RMSprop, SGD
-from utils.simple_data_generator import get_quadratic_data_sample, save_plot_to_buffer, plot_simple_data
+from utils.simple_data_generator import get_quadratic_data_sample, save_plot_to_buffer, plot_quadratic_data
+from utils.save_models import save_models
 from time import strftime, localtime
 
 
-class SimpleGan:
+def build_simple_nn(input_units, output_units, activation, layer_number=4, model_name='nn'):
+
         """
-        GAN for 1D Data: it tries to approximate simple quadratic data distribution
-        Creates Generator and Discriminator. Compiles them.
-        train_step(data): performs batch training on GAN
+        Build simple n layers fully connected neural network
+        :param input_units: int, number of input units
+        :param output_units: int, number of output units
+        :param activation: str, activation function
+        :return: Keras Model
         """
 
-        def __init__(self):
-                # Create generator and Discriminator
-                self.generator = self.build_generator((10,), 2)
-                self.discriminator = self.build_discriminator((2,))
+        # Input Layer
+        layer_name = model_name + '_input_layer'
+        nnin = Input(shape=(input_units,), name=layer_name)
 
-                # Compile Discriminator
-                self.discriminator.compile(
-                        optimizer=Adam(),
-                        loss='binary_crossentropy')
+        # First Hidden Layer
+        layer_name = model_name + '_hidden_layer_' + str(1)
+        nn = Dense(units=16, name=layer_name)(nnin)
+        nn = BatchNormalization()(nn)
+        nn = LeakyReLU(alpha=0.1)(nn)
 
-                # Create Gan Model: combine generator and discriminator
-                self.D_of_G = self.build_gan()
-                # Compile GAN model
-                self.D_of_G.compile(
-                        optimizer=Adam(),
-                        loss='binary_crossentropy')
+        for i in range(layer_number-1):
 
-        def build_generator(self, input_shape, output_units):
-                """
-                Returns generator neural network
-                :param input_shape: tuple input of generator
-                :param output_units: int number of output units
-                :return: Generator Keras Model
-                """
+                # New Hidden Layer
+                layer_name = model_name + '_hidden_layer_' + str(i + 2)
+                nn = Dense(units=16, name=layer_name)(nn)
+                nn = BatchNormalization()(nn)
+                nn = LeakyReLU(alpha=0.1)(nn)
 
-                # Input
-                gin = Input(
-                        shape=input_shape,
-                        name='generator_input')
+        # Output Layer
+        layer_name = model_name + '_output_layer'
+        nnout = Dense(
+                units=output_units,
+                activation=activation,
+                name=layer_name)(nn)
 
-                # Hidden Layers
-                g = Dense(units=16, kernel_initializer='he_uniform')(gin)
-                g = BatchNormalization()(g)
-                g = LeakyReLU(alpha=0.1)(g)
-                g = Dense(units=16, kernel_initializer='he_uniform')(g)
-                g = BatchNormalization()(g)
-                g = LeakyReLU(alpha=0.1)(g)
+        # Create Model
+        model = Model(nnin, nnout, name=model_name)
 
-                # Output
-                gout = Dense(
-                        units=output_units,
-                        name='generator_output',
-                        activation='linear'
-                )(g)
-                # Create Keras Model
-                model = Model(gin, gout, name='generator')
-                # Return Model
-                return model
+        # Return Model
+        return model
 
-        def build_discriminator(self, input_shape):
-                """
-                Returns Discriminator NN
-                :param input_shape: tuple shape generator input
-                :return: Discriminator Keras Model
-                """
 
-                # Input
-                din = Input(
-                        shape=input_shape,
-                        name='discriminator_input')
-                # Hidden Layers
-                d = Dense(units=16, kernel_initializer='he_uniform')(din)
-                d = LeakyReLU()(d)
-                d = BatchNormalization()(d)
-                d = Dense(units=16, kernel_initializer='he_uniform')(d)
-                d = BatchNormalization()(d)
+def build_gan(generator, discriminator):
 
-                d = LeakyReLU()(d)
+        # Get generator input
+        generator_input_dim = generator.input.shape[1]
+        # Freeze Discriminator Weights
+        discriminator.trainable = False
+        # Set Gan Input
+        gan_input = Input(
+                shape=(generator_input_dim,),
+                name='gan_input'
+        )
 
-                # Output
-                dout = Dense(
-                        units=1,
-                        activation='sigmoid',
-                        name='discriminator_output')(d)
+        # Set GAN Body (combine discriminator and generator)
+        # Set input on generator is necessary in Keras Model Class
+        gan_output = discriminator(generator(gan_input))
+        # Create Keras GAN Model
+        gan = Model(gan_input, gan_output, name='gan')
 
-                # Create Model
-                model = Model(din, dout, name='discriminator')
-                # Return Model
-                return model
+        # Return Model
+        return gan
 
-        def build_gan(self):
 
-                # Freeze Discriminator Weights
-                self.discriminator.trainable = False
-                # Set Gan Input
-                gan_input = Input(shape=(10,), name='gan_input')
-                # Set Gan Body (combine discriminator and generator)
-                # Set input on generator is necessary in Keras Model Class
-                gan_output = self.discriminator(self.generator(gan_input))
-                # Create Keras GAN Model
-                gan = Model(gan_input, gan_output)
-                # Return Model
-                return gan
+def train_step(data, generator, discriminator, d_of_g):
 
-        def train_step(self, data):
+        """
+        Performs Batch training on GAN
+        :param data: array of 2d coordinates, real data sample
+        :param generator: generator model
+        :param discriminator: discriminator model
+        :param d_of_g: gan model
+        :return: generator and discriminator losses
+        """
 
-                # Convert training data to tensor
-                real_data = tf.convert_to_tensor(data, dtype=tf.float32)
-                # Get Number of instances of real data == Batch size
-                batch_size = tf.shape(data)[0]
-                # Create z vectors to feed generator
-                random_z_vectors = tf.random.normal(shape=(batch_size, 10))
-                # Create generated data
-                generated_data = self.generator.predict_on_batch(random_z_vectors)
-                # Combine generated and real data to train on same batch
-                combined_data = tf.concat([real_data, generated_data], axis=0)
-                # Labels for real (ones vector) and fake (zeros vector) data
-                true_labels = tf.ones((batch_size, 1))
-                fake_labels = tf.zeros((batch_size, 1))
+        # Convert training data to tensor
+        real_data = tf.convert_to_tensor(data, dtype=tf.float32)
+        # Get Number of instances of real data == Batch size
+        batch_size = tf.shape(data)[0]
+        # Get generator input
+        generator_input_dim = generator.input.shape[1]
+        # Create z vectors to feed generator
+        random_z_vectors = tf.random.normal(shape=(batch_size, generator_input_dim))
+        # Create generated data
+        generated_data = generator.predict_on_batch(random_z_vectors)
+        # Combine generated and real data to train on same batch
+        combined_data = tf.concat([real_data, generated_data], axis=0)
+        # Labels for real (ones vector) and fake (zeros vector) data
+        true_labels = tf.ones((batch_size, 1))
+        fake_labels = tf.zeros((batch_size, 1))
 
-                # F Chollet Trick - Important!
-                # Adding noise to labels increases model generalization
-                true_labels += 0.05 * tf.random.uniform(tf.shape(true_labels))
-                fake_labels += 0.05 * tf.random.uniform(tf.shape(fake_labels))
+        # F Chollet Trick - Important!
+        # Adding noise to labels increases model generalization
+        true_labels += 0.05 * tf.random.uniform(tf.shape(true_labels))
+        fake_labels += 0.05 * tf.random.uniform(tf.shape(fake_labels))
 
-                # Combine labels for real and fake data
-                labels = tf.concat([true_labels,
-                                    fake_labels], axis=0)
+        # Combine labels for real and fake data
+        labels = tf.concat([true_labels,
+                            fake_labels], axis=0)
 
-                # START BATCH TRAINING
-                # Unfreeze discriminator weights
-                self.discriminator.trainable = True
+        # START BATCH TRAINING
+        # Unfreeze discriminator weights
+        discriminator.trainable = True
 
-                # Train discriminator and get loss for training tracking
-                discriminator_loss = self.discriminator.train_on_batch(combined_data, labels)
-                # Freeze discriminator weights to train generator
-                # Discriminator will be used only for inference here
-                self.discriminator.trainable = False
-                # Create random z vectors to feed generator
-                random_z_vectors = tf.random.normal(shape=(batch_size, 10))
-                # Train generator and get loss for training tracking
-                generator_loss = self.D_of_G.train_on_batch(random_z_vectors, true_labels)
+        # Train discriminator and get loss for training tracking
+        discriminator_loss = discriminator.train_on_batch(combined_data, labels)
+        # Freeze discriminator weights to train generator
+        # Discriminator will be used only for inference here
+        discriminator.trainable = False
+        # Create random z vectors to feed generator
+        random_z_vectors = tf.random.normal(shape=(batch_size, generator_input_dim))
+        # Train generator and get loss for training tracking
+        generator_loss = d_of_g.train_on_batch(random_z_vectors, true_labels)
 
-                # Return losses to track training
-                return discriminator_loss, generator_loss
+        # Return losses to track training
+        return discriminator_loss, generator_loss
+
+
 
 
 if __name__ == '__main__':
 
         # Set Parameters for training
-        # Training Epochs
-        epochs = 1001
-        # create gan model
-        gan = SimpleGan()
-        # Define Batch Size
-        batch_size = 100
-        # Every plot_epoch create a graph with real and generated data distribution
-        plot_interval = 100
+
+        epochs = 1001 # Training Epochs
+        z_dim = 10  # Generator Input units
+        layers = 4  # Number of hidden layers
+        g_out_dim = 2  # Generator Output Units == Discriminator Input Units
+        batch_size = 100  # Define Batch Size
+        plot_interval = 100  # Every plot_epoch create a graph with real and generated data distribution
+
+        # Build Generator
+        generator = build_simple_nn(
+                input_units=z_dim,
+                output_units=g_out_dim,
+                layer_number=layers,
+                activation='linear',
+                model_name='generator'
+        )
+        
+        # Build Discriminator
+        discriminator = build_simple_nn(
+                input_units=g_out_dim,
+                output_units=1,
+                layer_number=layers,
+                activation='sigmoid',
+                model_name='discriminator'
+        )
+
+        # Compile Discriminator
+        discriminator.compile(
+                optimizer=Adam(),
+                loss='binary_crossentropy')
+
+        # Create Keras GAN Model
+        gan = build_gan(generator, discriminator)
+
+        # Compile GAN Model
+        gan.compile(
+                optimizer=Adam(),
+                loss='binary_crossentropy')
 
         # Set Parameters for training tracking and training evaluation
         # Random vectors to control generator evolution
         control_z_vectors = tf.random.normal(shape=(batch_size, 10))
         # Returns quadratic distributed data, with and without noise
         # Take not noisy data
-        _, real_distribution = get_quadratic_data_sample(batch_size, x_min=-1, x_max=1)
+        real_distribution = get_quadratic_data_sample(batch_size, add_noise=False)
+
         # Set Tensorboard Directory to track training
         time = strftime("%d-%b-%H%M", localtime())
         log_dir = path.join('..', 'logs', 'simple_gan', time)
-        # generator_log_dir = path.join(log_dir, 'generator')
-        # discriminator_log_dir = path.join(log_dir, 'discriminator')
+
         # Initialize Keras metrics to track training
         generator_train_loss = tf.keras.metrics.Mean('generator_train_loss', dtype=tf.float32)
         discriminator_train_loss = tf.keras.metrics.Mean('discriminator_train_loss', dtype=tf.float32)
@@ -188,7 +195,7 @@ if __name__ == '__main__':
                 # START TRAINING
                 for e in range(epochs):
                         # Collect data
-                        _, real_data = get_quadratic_data_sample(batch_size, x_min=-1, x_max=1)
+                        real_data = get_quadratic_data_sample(batch_size)
                         # Train gan: Perform batch training with the collected data
                         d_loss, g_loss = gan.train_step(real_data)
                         # Save generator and discriminator losses
@@ -205,11 +212,9 @@ if __name__ == '__main__':
                                 # Points generated by generator with control vectors
                                 control_generated_data_points = gan.generator.predict_on_batch(control_z_vectors)
                                 # Plot Points generated by generator with control vectors
-                                control_generated_data_points_plot = plot_simple_data(
-                                        control_generated_data_points,
-                                        plot_type='points')
+                                plot_quadratic_data(control_generated_data_points, show=False)
                                 # Plot real distribution
-                                real_distribution_plot = plot_simple_data(real_distribution, plot_type='line')
+                                plot_quadratic_data(real_distribution, show=False)
                                 # Save plot image to buffer (no file creation needed)
                                 buf = save_plot_to_buffer()
                                 # Close plt figure
@@ -242,12 +247,4 @@ if __name__ == '__main__':
         tf.summary.trace_off()
 
         # Save Models
-        gan_name = 'gan_model_' + time + '.h5'
-        generator_name = 'generator_model_' + time + '.h5'
-        discriminator_name = 'discriminator_model_' + time + '.h5'
-        gan_path = path.join('weights', 'simple_gan', gan_name)
-        generator_path = path.join('weights', 'simple_gan', generator_name)
-        discriminator_path = path.join('weights', 'simple_gan', discriminator_name)
-        gan.D_of_G.save(gan_path)
-        gan.generator.save(generator_path)
-        gan.discriminator.save(discriminator_path)
+        save_models(discriminator, generator, gan)
--- utils/simple_data_generator.py
+++ utils/simple_data_generator.py
@@ -1,55 +1,68 @@
 import io
 
-from numpy.random import exponential, sample, random
+from numpy.random import exponential, sample, random, uniform
 from numpy import arange, power, polyfit, poly1d, sort, array, float32
-
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def get_quadratic_data_sample(n, x_min=-10, x_max=10, noise=10, pow=2):
+def get_quadratic_data_sample(n, add_noise=False, x_min=-1, x_max=1, noise=0.01, pow=2):
 
         """
         Create random sample of n points on parabolic distribution with some noise
         :param n: int, number of points
+        :param add_noise: bool, whether to add noise to y coordinate
         :param x_min: float, min value of sample
         :param x_max: float, max value of sample
         :param noise: float, multiplier of random divergence from trend
         :param pow: int, parabolic power
-        :return: two arrays of x and y coordinates with and without added noise
+        :return: numpy array shape (n, 2), x and y coordinates
         """
 
         ### Sanity Checks ###
-
         if type(n) is not int or n <= 0: raise Exception("Sample size must be a positive integer")
         if type(pow) is not int or pow % 2 != 0 or pow <= 0: raise Exception("Power must be a positive even integer ")
         if x_min >= x_max: raise Exception("Min value ({}) is higher or equal than max value ({})".format(x_min, x_max))
 
-
+        x = uniform(x_min, x_max, n)  # Points along x
         mid = (x_max-x_min)/2 + x_min  # Middle point
-        x = (x_max-x_min) * random(n) + x_min  # Points along x
-        x = sort(x)  # Sort x
-        y = power((x - mid), pow)  # Raise x
+        y = power((x-mid), pow)  # Raise x
 
-        y_noise = y + (2 * random(n) - 1) * noise # Add noise to y
+        if add_noise: y += (2 * random(n) - 1) * noise  # Add noise to y
 
-        noisy_points_array = array(list(zip(x, y_noise)), dtype=float32)
-        clean_points_array = array(list(zip(x, y)), dtype=float32)
-
-        return noisy_points_array, clean_points_array
+        # combine x and y as points in 2D plane
+        coordinates = list(zip(x,y))
+        # Return coordinates as numpy array
+        return array(coordinates, dtype=float32)
 
 
-def plot_simple_data(data, plot_type='line', show=False):
+def plot_quadratic_data(data, show=True, pow=2):
 
-        x = data[:, 0]
-        y = data[:, 1]
-        # plot = None
-        if plot_type == 'line': plot = plt.plot(x, y)
-        elif plot_type == 'points': plot = plt.plot(x, y, '.')
-        else: raise Exception('Arg "plot_type" must be "line" or "points", {} format not supported'.format(plot_type))
-        if show: plt.show()
-        return plot
+        """
+        Creates a plt axes with datapoints in data
+        :param data: numpy array shape (n, 2), x and y coordinates
+        :param show: bool, whether to show the graph or not
+        :param pow: int, parable exponent
+        :return:
+        """
+
+        if type(pow) is not int or pow % 2 != 0 or pow <= 0: raise Exception("Power must be a positive even integer")
+
+        x = data[:, 0]  # unzip x
+        y = data[:, 1]  # unzip y
+        ax = sns.regplot(x, y, order=pow)  # Plot
+        if show: plt.show()  # Show if True
+        return ax  # Return plt axes
+
 
 def save_plot_to_buffer():
+
+        """
+        takes all active figure plots and adds them into memory
+        its useful and computationally cheaper not save them
+        when plotting in Tensorboard
+        :return: memory object
+        """
 
         buf = io.BytesIO()
         plt.savefig(buf, format='png')
@@ -57,15 +70,8 @@ def save_plot_to_buffer():
         return buf
 
 
-
-
-
-
 if __name__ == '__main__':
 
-        noisy_data, clean_data = get_quadratic_data_sample(100)
-
-
-        plot_noisy_data = plot_simple_data(noisy_data, plot_type='points')
-        plot_clean_data = plot_simple_data(clean_data, plot_type='line')
-        buf = save_plot_to_buffer()
+        clean_data = get_quadratic_data_sample(100)
+        plot = plot_quadratic_data(clean_data)
+        # plt.show()
