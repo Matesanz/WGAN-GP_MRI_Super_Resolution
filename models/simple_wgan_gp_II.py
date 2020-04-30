53e9d81a0b9953986e30f43e9fbb3d90ff15d4b2
Matesanz <44867923+matesanz@users.noreply.github.com>
2020-04-30 16:17:03 +0200

    Divide generator and critic training

--- models/simple_wgan_gp_II.py
+++ models/simple_wgan_gp_II.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 class Simple_WGAN(Model):
 
-        def __init__(self, layers_n, hidden_units, gout_units=2, z_units=10, g_lr=5e-5, c_lr=5e-5):
+        def __init__(self, layers_n, hidden_units, gout_units=2, z_units=10, g_lr=5e-5, c_lr=5e-5, gradient_penalty_weight = 10):
                 super(Simple_WGAN, self).__init__()
 
 
@@ -23,6 +23,7 @@ class Simple_WGAN(Model):
                 self.hidden_units = hidden_units  # units on hidden layers
                 self.g_lr = g_lr  # generator learning rate
                 self.c_lr = c_lr  # discriminator learning rate
+                self.gradient_penalty_weight = gradient_penalty_weight  # interpolated image loss weight
                 # critic_loops = 5  # number of iterations to train discriminator on every epoch
 
                 self.generator_optimizer = Adam(lr=self.g_lr, beta_1=0.5)
@@ -53,17 +54,79 @@ class Simple_WGAN(Model):
                 for layer in model.layers:
                         layer.trainable = val
 
-        def compute_loss(self, data):
+        def gradient_penalty_loss(self, real_data, generated_data):
+
+                # Get Number of instances of real data == Batch size
+                batch_size = real_data.shape[0]
+                alpha = tf.random.uniform((batch_size, 1))
+                inter_data = (alpha * real_data) + ((1 - alpha) * generated_data)
+
+                with tf.GradientTape() as g:
+
+                        g.watch(inter_data)
+                        logits_inter_data = self.critic(inter_data)
+
+                gradients = g.gradient(logits_inter_data, inter_data)
+
+                # compute the euclidean norm by squaring ...
+                gradients_sqr = tf.square(gradients)
+                #   ... summing over the rows ...
+                gradients_sqr_sum = tf.reduce_sum(gradients_sqr, axis=0)
+                #   ... and sqrt
+                gradient_l2_norm = tf.sqrt(gradients_sqr_sum)
+                # compute lambda * (1 - ||grad||)^2 still for each single sample
+                gradient_penalty = tf.square(1 - gradient_l2_norm)
+                # return the mean as loss over all the batch samples
+                return tf.reduce_mean(gradient_penalty)
+
+        def generate_data(self, n):
+
+                # Create random z vectors to feed generator
+                random_z_vectors = tf.random.normal(shape=(n, self.z_units))
+                generated_data = self.generator(random_z_vectors)
+                return generated_data
+
+        def compute_generator_loss(self, batch_size):
+
+                # Get fake data to feed generator
+                generated_data = self.generate_data(batch_size)
+                # feed generator and get logits
+                logits_generated_data = self.critic(generated_data)
+                # losses of fake with label "1"
+                generator_loss = tf.reduce_mean(logits_generated_data)
+
+                return generator_loss
+
+        def compute_generator_gradients(self, batch_size):
+
+                with tf.GradientTape() as gen_tape:
+                        gen_loss = self.compute_generator_loss(batch_size)
+                # compute gradients
+                gen_gradients = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
+                return gen_gradients
+
+        def apply_generator_gradients(self, gradients):
+
+                """
+                Apply calculated gradients to update generator
+                :param gradients: generator gradients
+                """
+                # Optimizer applies gradients on trainable weights
+                self.generator_optimizer.apply_gradients(
+                        zip(gradients, self.generator.trainable_variables)
+                )
+
+        def compute_critic_loss(self, real_data):
 
                 """
                 passes through the network and computes loss
                 """
 
                 # Get Number of instances of real data == Batch size
-                batch_size = tf.shape(data)[0]
+                batch_size = real_data.shape[0]
 
                 # Convert numpy training data to tensor
-                real_data = tf.convert_to_tensor(data, dtype=tf.float32)
+                real_data = tf.convert_to_tensor(real_data, dtype=tf.float32)
 
                 # Create random z vectors to feed generator
                 random_z_vectors = tf.random.normal(shape=(batch_size, self.z_units))
@@ -81,44 +144,12 @@ class Simple_WGAN(Model):
                         tf.reduce_mean(logits_real_data)
                         - tf.reduce_mean(logits_generated_data)
                         + critic_regularizer
-                        # * self.gradient_penalty_weight
-                        * 10
+                        * self.gradient_penalty_weight
                 )
 
-                # losses of fake with label "1"
-                generator_loss = tf.reduce_mean(logits_generated_data)
+                return critic_loss
 
-                return critic_loss, generator_loss
-
-        def gradient_penalty_loss(self, real_data, generated_data):
-
-                # Get Number of instances of real data == Batch size
-                batch_size = tf.shape(real_data)[0]
-                alpha = tf.random.uniform((batch_size, 1))
-                inter_data = (alpha * real_data) + ((1 - alpha) * generated_data)
-
-                with tf.GradientTape() as g:
-
-                        g.watch(inter_data)
-                        logits_inter_data = self.critic(inter_data)
-
-                gradients = g.gradient(logits_inter_data, inter_data)
-
-                # compute the euclidean norm by squaring ...
-                gradients_sqr = tf.square(gradients)
-                #   ... summing over the rows ...
-                gradients_sqr_sum = tf.reduce_sum(gradients_sqr, axis=0)
-                # gradients_sqr_sum = tf.reduce_sum(gradients_sqr,
-                #                           axis=arange(1, len(gradients_sqr.shape)))
-                #   ... and sqrt
-                gradient_l2_norm = tf.sqrt(gradients_sqr_sum)
-                # compute lambda * (1 - ||grad||)^2 still for each single sample
-                gradient_penalty = tf.square(1 - gradient_l2_norm)
-                # return the mean as loss over all the batch samples
-
-                return tf.reduce_mean(gradient_penalty)
-
-        def compute_gradients(self, real_data):
+        def compute_critic_gradients(self, real_data):
 
                 """
                 Compute Gradients to update generator and discriminator
@@ -127,36 +158,36 @@ class Simple_WGAN(Model):
                 """
 
                 # pass through network
-                with tf.GradientTape() as gen_tape, tf.GradientTape() as critic_tape:
-                        critic_loss, gen_loss = self.compute_loss(real_data)
+                with tf.GradientTape() as critic_tape:
+                        critic_loss = self.compute_critic_loss(real_data)
 
                 # compute gradients
-                gen_gradients = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
                 critic_gradients = critic_tape.gradient(critic_loss, self.critic.trainable_variables)
 
-                return gen_gradients, critic_gradients
+                return critic_gradients
 
-        def apply_gradients(self, generator_gradients, critic_gradients):
+        def apply_critic_gradients(self, gradients):
 
                 """
-                Apply calculated gradients to update networks
-                :param generator_gradients: gen gradients
-                :param critic_gradients: critic gradients
+                Apply calculated gradients to update critic
+                :param gradients: critic gradients
                 """
 
                 # Optimizer applies gradients on trainable weights
-                self.generator_optimizer.apply_gradients(
-                        zip(generator_gradients, self.generator.trainable_variables)
-                )
-
                 self.critic_optimizer.apply_gradients(
-                        zip(critic_gradients, self.critic.trainable_variables)
+                        zip(gradients, self.critic.trainable_variables)
                 )
 
         @tf.function
-        def train(self, real_data):
-                gen_gradients, disc_gradients = self.compute_gradients(real_data)
-                self.apply_gradients(gen_gradients, disc_gradients)
+        def train_generator(self, batch_size):
+                gen_gradients = self.compute_generator_gradients(batch_size)
+                self.apply_generator_gradients(gen_gradients)
+
+        @tf.function
+        def train_critic(self, real_data):
+                critic_gradients = self.compute_critic_gradients(real_data)
+                self.apply_critic_gradients(critic_gradients)
+
 
 
 if __name__ == '__main__':
@@ -170,8 +201,9 @@ if __name__ == '__main__':
                 hidden_units=16,
         )
         batch_size = 64  # Samples every epoch
-        n_epochs = 10001  # Training Epochs
+        n_epochs = 1001  # Training Epochs
         plot_interval = 10  # Every plot_interval create a graph with real and generated data distribution
+        c_loops = 5  # number of loops to train critic every epoch
 
         # fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(8.5, 4))
         # fig.suptitle("Simple Wasserstein GP GAN Training Evolution", size=20)
@@ -187,7 +219,7 @@ if __name__ == '__main__':
         log_dir = path.join('..', 'logs', 'simple_wgan', time)
         # Start model training tracing (logs)
         summary_writer = tf.summary.create_file_writer(log_dir)
-        tf.summary.trace_on(graph=True, profiler=True)
+        # tf.summary.trace_on(graph=True, profiler=True)
 
         z_control = tf.random.normal((batch_size, wgan.z_units))
         real_distribution = get_quadratic_data_sample(batch_size)
@@ -198,9 +230,15 @@ if __name__ == '__main__':
                 #     TRAINING
                 # --------------------
 
-                training_data = get_quadratic_data_sample(batch_size)  # Get points from real distribution
-                wgan.train(training_data)  # Train our model on real distribution points
-                c_loss, g_loss = wgan.compute_loss(training_data)  # Get batch loss to track training
+                # Train Critic
+                for _ in range(c_loops):
+                        training_data = get_quadratic_data_sample(batch_size)  # Get points from real distribution
+                        wgan.train_critic(training_data)
+
+                # Train Generator
+                wgan.train_generator(batch_size)  # Train our model on real distribution points
+                c_loss= wgan.compute_critic_loss(training_data)  # Get batch loss to track training
+                g_loss = wgan.compute_generator_loss(batch_size)
 
                 # -----------------------
                 #  TENSORBOARD TRACKING
