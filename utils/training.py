823d5dc314e18ec21edb07eb2daf64adbc1d550a
Matesanz <Matesanz.Cuadrado@gmail.com>
2020-05-12 11:59:14 +0200

    batch generator loops over data

--- utils/training.py
+++ utils/training.py
@@ -1,6 +1,7 @@
 from time import strftime, localtime
 from os import path, listdir
 from utils.imutils import process_mnc
+from numpy import asarray
 
 
 def save_models(d, g, d_of_g, folder_name):
@@ -21,25 +22,34 @@ def save_models(d, g, d_of_g, folder_name):
 class DataGenerator:
 
         def __init__(self, folder_path):
+                self.folder_path = folder_path
                 self.data_generator = (process_mnc(path.join(folder_path, filename)) for filename in listdir(folder_path))
+                self.data_length = len(listdir(folder_path))
+                self.data_taken = 0
 
         def get_batch(self, batch_size):
 
                 batch = []
                 for _ in range(batch_size):
 
+                        if self.data_taken >= self.data_length: self.reset_generator()
                         sample = next(self.data_generator)
                         batch.append(sample)
+                        self.data_taken += 1
 
-                return batch
+                return asarray(batch)
+
+        def reset_generator(self):
+                # print("generator reseted")
+                self.data_taken = 0
+                self.data_generator = (
+                        process_mnc(path.join(self.folder_path, filename)) for filename in listdir(self.folder_path)
+                )
+
 
 
 if __name__ == '__main__':
 
         PATH = path.join('..', 'resources', 'mri')
         data_generator = DataGenerator(PATH)
-
         data_generator.get_batch(2)
-        data_generator.get_batch(2)
-        data_generator.get_batch(2)
-
